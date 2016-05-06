341f2bba7cb1c153298dfc769c945144351d31a7
Hanks <hankszhang918@gmail.com>
2016-05-06 14:55:03 +0800

    Implement several kinds of advanced sorting algorithms
    Signed-off-by: Hanks <hankszhang918@gmail.com>

--- js/algorithm/sort.js
+++ js/algorithm/sort.js
@@ -51,11 +51,47 @@ function insertionSort(array){
     }
 }
 
-// Test cases
-/*var numElements = 100000;
+//希尔排序
+//希尔排序基于插入排序，通过使用不同的间隔来提高效率
+function shellSort(array, gap){
+    var count = 0;
+    for(var g=0; g<gap.length; g++){
+        //print("The gap is: " + gap[g]);
+        for(var outer=gap[g]; outer<array.length; outer++){
+            var temp = array[outer],
+                inner = outer;
+            while(inner>=gap[g] && (array[inner-gap[g]])>temp){
+                array[inner] = array[inner-gap[g]];
+                inner -= gap[g];
+            }
+            array[inner] = temp;
+            //count++;
+            //print("Step " + count + ": " + array);
+        }
+    }
+}
+
+//快速排序
+function quickSort(array){
+    if (array.length == 0) {
+        return [];
+    }
+    var left = [],
+        right = [],
+        pivot = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] < pivot) {
+            left.push(array[i]);
+        } else {
+            right.push(array[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+var numElements = 1000000;
 var mynums = new CArray(numElements);
 mynums.setData();
-//print("Original array: " + mynums.toString());
 var nums1 = [],
     nums2 = [];
 for(var i=0; i<mynums.data.length; i++){
@@ -64,13 +100,16 @@ for(var i=0; i<mynums.data.length; i++){
 for(var j=0; j<mynums.data.length; j++){
     nums2[j] = mynums.data[j];
 }
-var start = new Date().getTime();
-bubbleSort(mynums.data);
-var stop = new Date().getTime();
-var time = stop - start;
-print("Sorted by bubble algorithm:");
-print("Time used: " + time)
+// Test cases for basic sorting algorithm
+//print("Original array: " + mynums.toString());
+//var start = new Date().getTime();
+//bubbleSort(mynums.data);
+//var stop = new Date().getTime();
+//var time = stop - start;
+//print("Sorted by bubble algorithm:");
+//print("Time used: " + time)
 //print(mynums.toString());
+/*
 start = new Date().getTime();
 selectionSort(nums1);
 var stop = new Date().getTime();
@@ -85,3 +124,19 @@ time = stop - start;
 print("Sorted by insertion algorithm:");
 print("Time used: " + time)
 //print(mynums.toString());*/
+
+// Test cases for advanced sorting algorithm
+var gap = [701,301,132,57,23,10,4,1];
+start = new Date().getTime()
+shellSort(nums1, gap);
+stop = new Date().getTime();
+time = stop - start;
+print("Sorted by shellSort algorithm:");
+print("Time used: " + time)
+start = new Date().getTime()
+var res = quickSort(nums2);
+stop = new Date().getTime();
+time = stop - start;
+print("Sorted by quickSort algorithm:");
+print("Time used: " + time)
+//print(res);
